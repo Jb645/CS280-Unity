fd99b7eb81406d70c545a9f54233682875f2f38c
Jb645 <50148003+Jb645@users.noreply.github.com>
2021-07-19 14:29:41 -0400

    testing

---
+++ Introduction/Assets/Scripts/Hero.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+namespace SchoolPractice
+{
+    public class Hero : MonoBehaviour
+    {
+        [SerializeField] private float speed = 3f;
+        
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+            
+            
+            
+        }
+    }
+}
